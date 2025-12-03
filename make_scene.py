27b2c00e992f89549c5b554bbaadb7849927b96c
Jin Woo Kong <121531442+wlsdnqqq17@users.noreply.github.com>
2025-12-03 22:23:29 +0900

    Update make_scene.py

--- make_scene.py
+++ make_scene.py
@@ -1,10 +1,18 @@
 import math
+import os
+import platform
 import random
 from pathlib import Path
 
 import bpy
 
 FILE_PATH = Path("/Users/jinwoo/Documents/work/room")
+system = platform.system()
+print(system)
+if system == "Linux":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+FILE_PATH = Path(__file__).parent
+
 TOTAL_FRAMES = 320
 FRAME_STEP = 20
 MOVE_SPEED = 1.0
@@ -33,8 +41,11 @@ add_cube("wall3", (7, -1, 0), (0.1, 1, 5))
 add_cube("wall4", (-3, -1, 0), (0.1, 1, 5))
 
 bpy.ops.object.camera_add(location=(-1, -1, -2))
+bpy.ops.object.camera_add(location=(-1, -1, -1.5))
 cam = bpy.context.active_object
 cam.rotation_euler = (0, -2.14675, 3.14159)
+cam.rotation_euler = (math.radians(0), math.radians(-109), math.radians(180))
+bpy.context.scene.camera = cam
 
 bpy.ops.object.light_add(type="POINT", location=(2, -1.9, 2.5))
 bpy.context.object.data.energy = 200
@@ -114,6 +125,25 @@ while frame < TOTAL_FRAMES:
 scene = bpy.context.scene
 
 scene.render.engine = "CYCLES"
+
+preferences = bpy.context.preferences
+cycles_prefs = preferences.addons["cycles"].preferences
+
+if system == "Linux":
+    cycles_prefs.compute_device_type = "CUDA"
+
+    cycles_prefs.get_devices()
+    found_gpu = False
+
+    for device in cycles_prefs.devices:
+        if device.type == "CUDA" or device.type == "OPTIX":
+            device.use = True
+            found_gpu = True
+            print(f"Using GPU: {device.name}")
+        else:
+            device.use = False
+            print(f"Not using GPU: {device.name}")
+
 scene.cycles.device = "GPU"
 scene.cycles.samples = 128
 scene.cycles.use_denoising = True
@@ -121,3 +151,34 @@ scene.cycles.use_denoising = True
 scene.render.resolution_x = 256
 scene.render.resolution_y = 256
 scene.render.resolution_percentage = 100
+
+scene.render.image_settings.file_format = "FFMPEG"
+scene.render.ffmpeg.format = "MPEG4"
+scene.render.ffmpeg.codec = "H264"
+scene.render.ffmpeg.constant_rate_factor = "MEDIUM"
+scene.render.ffmpeg.audio_codec = "NONE"
+
+scene.render.filepath = str(FILE_PATH / "output.mp4")
+scene.frame_start = 1
+scene.frame_end = TOTAL_FRAMES
+
+print(f"Start Rendering to {FILE_PATH}...")
+bpy.ops.render.render(animation=True)
+print("Render Finished!")
+
+blend_file_path = FILE_PATH / "saved_scene.blend"
+bpy.ops.wm.save_mainfile(filepath=str(blend_file_path))
+
+bpy.ops.object.camera_add(location=(2, -10, 0))
+cam = bpy.context.active_object
+cam.rotation_euler = (math.radians(90), 0, 0)
+bpy.context.scene.camera = cam
+cam.data.lens = 23
+
+if "ceiling" in bpy.data.objects:
+    bpy.data.objects["ceiling"].hide_render = True
+
+scene.render.filepath = str(FILE_PATH / "output_top.mp4")
+print(f"Start Rendering to {FILE_PATH / 'output_top.mp4'}...")
+bpy.ops.render.render(animation=True)
+print(f"Render Finished to {FILE_PATH / 'output_top.mp4'}!")
